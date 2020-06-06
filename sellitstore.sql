ae5d6589a6b8bfb5e9dec82215643212679b2953
Keshav Singh <keshavsingh200100@gmail.com>
2020-06-06 15:25:06 +0530

    dump file

--- sellitstore.sql
+++ sellitstore.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `auth_group`
 --
-
+ 
 DROP TABLE IF EXISTS `auth_group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
